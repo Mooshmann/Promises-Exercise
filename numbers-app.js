0b7e7a6a0cb701cc7b9d5c89beb08bf3a7eb325c
Rich Beilfuss <rdebeilfuss@gmail.com>
2022-04-19 14:57:05 -0500

    add files to repo

---
+++ numbers-app.js
@@ -0,0 +1,19 @@
+let favNum = 13;
+let baseURL = "http://numbersapi.com";
+
+$.getJSON(`${baseURL}/${favNum}?json`).then(data => {
+    console.log(data);
+})
+
+let favNums = [6,13,21];
+$.getJSON(`${baseURL}/${favNums}?json`).then(data => {
+    console.log(data);
+})
+
+Promise.all(
+    Array.from({ length: 4}, () => {
+        return $.getJSON(`${baseURL}/${favNum}?json`);
+    })
+).then(facts => {
+    facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+});
